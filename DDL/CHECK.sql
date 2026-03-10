5e4c1ae260b2db1f4c3f52fa8f6871791ee376ca
Egor-milk <mrpixelegor@mail.ru>
2026-03-10 13:04:26 +0700

    Create CHECK.sql

---
+++ DDL/CHECK.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book
+(
+	book_id int,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL,
+	publisher_id int,
+
+	CONSTRAINT PK_book_book_id PRIMARY KEY(book_id)
+);
+
+ALTER TABLE book
+ADD COLUMN price decimal 
+	CONSTRAINT CHK_book_price CHECK(price >= 0);
+
+
+INSERT INTO book
+VALUES
+(1, 'title', 'ismn', 1, 1)
+
+SELECT * FROM book
